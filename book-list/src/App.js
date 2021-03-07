48aae01833d40c111922d546e20c52c28abdc6e1
abruno92 <abruno92@github.com>
2021-03-07 16:08:39 +0100

    added toggle button, and auth context to togle log in and ut

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -1,13 +1,18 @@
 import BookList from './components/BookList';
 import Navbar from './components/NavBar';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './context/AuthContext';
 import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
 	return (
 		<div className='App'>
 			<ThemeContextProvider>
-				<Navbar />
-				<BookList />
+				<AuthContextProvider>
+					<Navbar />
+					<BookList />
+					<ThemeToggle />
+				</AuthContextProvider>
 			</ThemeContextProvider>
 		</div>
 	);
